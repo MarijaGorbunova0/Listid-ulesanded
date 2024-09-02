06cebf326974b322ee26b38733cf1b8b74f1002c
opilane <opilane@HP-CZC2349HTS>
2024-09-02 15:47:19 +0300

    Add project files.

---
+++ Listid-ülesanded/Func.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listid_ulesanded
+{
+    public static class Funct
+    {
+        //2 
+        static Dictionary<string, string> counties = new Dictionary<string, string>
+            {
+                    { "Таллин", "Харью" },
+                    { "Тарту", "Тартумаа" },
+                    { "Пярну", "Пярнумаа" },
+            };
+        public static List<int> RandomNumb(int number)
+        {
+            Random rand = new Random();
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < number; i++)
+            {
+                numbers.Add(rand.Next(1, 101));
+            }
+
+            return numbers;
+        }
+        public static void Numbers()
+        {
+            Console.WriteLine("sis num");
+            string numberstr = Console.ReadLine(); 
+        }
+        public static void Linnad()
+        {
+            bool flag = false;
+            Console.WriteLine("sis number");
+            string userinput = Console.ReadLine();
+            if (counties.ContainsKey(userinput))
+            {
+                Console.WriteLine($"Столица {userinput} находится в округе {counties[userinput]}");
+            }
+            else if(counties.ContainsValue(userinput))
+            {
+                foreach (var vls in counties) 
+                {
+                    if (vls.Value == userinput)
+                    {
+                        Console.WriteLine($"Округ {userinput} имеет столицу {vls.Key}"); 
+                        flag = true;
+                    }
+
+                }
+                if (!flag)
+                {
+                    Console.WriteLine("kas te tahate uue sisetada")
+                }
+            }
+        }
+    }
+}
+
---
+++ Listid-ülesanded/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Listid_ulesanded
+{
+    public static class Programm
+    {
+        public static void Main()
+        {
+            //2
+            List<int> randomNumbers = Funct.RandomNumb(20);
+            Console.WriteLine("Исходный список чисел:");
+            randomNumbers.ForEach(n => Console.Write(n + " "));
+
+           
+            List<int> evenNumbers = randomNumbers.Where(n => n % 2 == 0).ToList();
+            List<int> oddNumbers = randomNumbers.Where(n => n % 2 != 0).ToList();
+
+           
+            List<int> sortNumbers = evenNumbers.Concat(oddNumbers).ToList();
+
+            
+            Console.WriteLine("\n\nСписок после сортировки:");
+            sortNumbers.ForEach(n => Console.Write(n + " "));
+        }
+    }
+}
